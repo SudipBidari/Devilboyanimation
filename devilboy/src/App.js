e2d6d4612f47c86b981ec53def4ab38cd2628d39
SudipBidari <spidun118@gmail.com>
2023-07-27 00:11:09 +0545

    Updated

--- devilboy/src/App.js
+++ devilboy/src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-
+import './Home'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello World!
-      </header>
+      <Home />
     </div>
   );
 }
---
+++ devilboy/src/Home.js
@@ -0,0 +1,13 @@
+import './Home.css'
+
+function Home() {
+    return(
+        <div className = "Home">
+            <div className="dba_logo">
+                
+            </div>
+        </div>
+    );
+  }
+
+export default Home;
